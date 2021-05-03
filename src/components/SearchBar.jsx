0b30ff6512cbd4f3645393099ed1814112b16298
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-05-03 15:14:42 -0300

    1 - Crie um componente chamado <SearchBar /> ✅

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,13 @@
-// implement AddMovie component here
+import React, { Component } from 'react'
+
+class SearchBar extends Component {
+  render() {
+    // const { searchText, onSearchTextChange, }
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+export default SearchBar;
