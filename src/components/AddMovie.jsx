4cb6621296ee8d2666f93f82b3a339c2ed128865
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-05-03 22:08:09 -0300

    13 - Renderize um select do formulário em <AddMovie /> para selecionar o gênero do novo filme ✅

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -86,6 +86,21 @@ class AddMovie extends Component {
           />
         </label>
         <br />
+        <label htmlFor="genre" data-testid="genre-input-label">
+          Gênero
+          <select
+            id="genre"
+            name="genre"
+            value={ genre }
+            data-testid="genre-input"
+            onChange={ this.handleChange }
+          >
+            <option value="action" data-testid="genre-option">Ação</option>
+            <option value="comedy" data-testid="genre-option">Comédia</option>
+            <option value="thriller" data-testid="genre-option">Suspense</option>
+          </select>
+        </label>
+        <br />
       </form>
     );
   }
