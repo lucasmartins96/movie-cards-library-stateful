442addb635e1fc020d4952149e176c0801cdb16a
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-05-08 18:11:11 -0300

    Refatorando o Componente AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,9 +1,5 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import RowInputNumber from './my-components/RowInputNumber';
-import RowInputText from './my-components/RowInputText';
-import RowSelect from './my-components/RowSelect';
-import RowTextArea from './my-components/RowTextArea';
 
 const INITIAL_STATE = {
   subtitle: '',
@@ -30,46 +26,137 @@ class AddMovie extends Component {
     this.setState(INITIAL_STATE);
   }
 
+  renderTitleInput() {
+    const { title } = this.state;
+
+    return (
+      <div>
+        <label htmlFor="title" data-testid="title-input-label">
+          Título
+          <input
+            type="text"
+            id="title"
+            name="title"
+            value={ title }
+            data-testid="title-input"
+            onChange={ this.handleChange }
+          />
+        </label>
+      </div>
+    );
+  }
+
+  renderSubtitleInput() {
+    const { subtitle } = this.state;
+
+    return (
+      <div>
+        <label htmlFor="subtitle" data-testid="subtitle-input-label">
+          Subtítulo
+          <input
+            type="text"
+            id="subtitle"
+            name="subtitle"
+            value={ subtitle }
+            data-testid="subtitle-input"
+            onChange={ this.handleChange }
+          />
+        </label>
+      </div>
+    );
+  }
+
+  renderImagePathInput() {
+    const { imagePath } = this.state;
+
+    return (
+      <div>
+        <label htmlFor="image" data-testid="image-input-label">
+          Imagem
+          <input
+            type="text"
+            id="image"
+            name="imagePath"
+            value={ imagePath }
+            data-testid="image-input"
+            onChange={ this.handleChange }
+          />
+        </label>
+      </div>
+    );
+  }
+
+  renderRatingInput() {
+    const { rating } = this.state;
+
+    return (
+      <div>
+        <label htmlFor="rating" data-testid="rating-input-label">
+          Subtítulo
+          <input
+            type="number"
+            id="rating"
+            name="rating"
+            value={ rating }
+            data-testid="rating-input"
+            onChange={ this.handleChange }
+          />
+        </label>
+      </div>
+    );
+  }
+
+  renderStorylineTextarea() {
+    const { storyline } = this.state;
+
+    return (
+      <div>
+        <label htmlFor="storyline" data-testid="storyline-input-label">
+          Sinopse
+          <textarea
+            id="storyline"
+            name="storyline"
+            value={ storyline }
+            data-testid="storyline-input"
+            onChange={ this.handleChange }
+          />
+        </label>
+      </div>
+    );
+  }
+
+  renderGenreSelect() {
+    const { genre } = this.state;
+
+    return (
+      <div>
+        <label htmlFor="genre" data-testid="genre-input-label">
+          Gênero
+          <select
+            id="genre"
+            name="genre"
+            value={ genre }
+            data-testid="genre-input"
+            onChange={ this.handleChange }
+          >
+            <option value="action" data-testid="genre-option">Ação</option>
+            <option value="comedy" data-testid="genre-option">Comédia</option>
+            <option value="thriller" data-testid="genre-option">Suspense</option>
+          </select>
+        </label>
+      </div>
+    );
+  }
+
   render() {
-    const { title, subtitle, imagePath, storyline, rating, genre } = this.state;
     return (
       <form data-testid="add-movie-form">
-        <RowInputText
-          propsAux={ ['title', 'title', title, this.handleChange] }
-        >
-          Título
-        </RowInputText>
-        <br />
-        <RowInputText
-          propsAux={ ['subtitle', 'subtitle', subtitle, this.handleChange] }
-        >
-          Subtítulo
-        </RowInputText>
-        <br />
-        <RowInputText
-          propsAux={ ['image', 'imagePath', imagePath, this.handleChange] }
-        >
-          Imagem
-        </RowInputText>
-        <br />
-        <RowTextArea
-          propsAux={ ['storyline', 'storyline', storyline, this.handleChange] }
-        >
-          Sinopse
-        </RowTextArea>
-        <br />
-        <RowInputNumber
-          propsAux={ ['rating', 'rating', rating, this.handleChange] }
-        >
-          Avaliação
-        </RowInputNumber>
-        <br />
-        <RowSelect
-          propsAux={ ['genre', 'genre', genre, this.handleChange] }
-        >
-          Gênero
-        </RowSelect>
-        <br />
+        { this.renderTitleInput() }
+        { this.renderSubtitleInput() }
+        { this.renderImagePathInput() }
+        { this.renderRatingInput() }
+        { this.renderStorylineTextarea() }
+        { this.renderGenreSelect() }
         <button
           type="button"
           data-testid="send-button"
